bb4db0c004e2eb87a9406287d56063153ee59370
edvillones <secret.sucesscode@gmail.com>
2024-10-27 12:16:15 +0800

    main.js

--- main.js
+++ main.js
@@ -4,14 +4,11 @@ class AcodePlugin {
     this.initialDistance = 0;
     this.initialScale = 1;
     this.currentScale = 15;
+    this.minimum = 10;
+    this.maximum = 40;
 
     this.start = this.start.bind(this);
     this.move = this.move.bind(this);
-  }
-
-  async init() {
-    document.documentElement.addEventListener("touchstart", this.start);
-    document.documentElement.addEventListener("touchmove", this.move);
   }
 
   getDistance(touch1, touch2) {
@@ -31,32 +28,29 @@ class AcodePlugin {
     if (e.touches.length === 2) {
       e.preventDefault(); // Prevent page scrolling
       const currentDistance = this.getDistance(e.touches[0], e.touches[1]);
-      this.currentScale =  this.initialScale * (currentDistance / this.initialDistance);
+      this.currentScale =
+        this.initialScale * (currentDistance / this.initialDistance);
 
-      if (this.currentScale <= 10) this.currentScale = 10;
-      else if (this.currentScale >= 40) this.currentScale = 40;
+      if (this.currentScale <= this.minimum) this.currentScale = this.minimum;
+      else if (this.currentScale >= this.maximum)
+        this.currentScale = this.maximum;
 
       this.settings.value.fontSize = this.currentScale + "px";
       this.settings.update(false);
     }
   }
 
-  async destroy() {
+  init() {
+    document.documentElement.addEventListener("touchstart", this.start);
+    document.documentElement.addEventListener("touchmove", this.move);
+  }
+  
+  destroy() {
     document.documentElement.removeEventListener("touchstart", this.start);
     document.documentElement.removeEventListener("touchmove", this.move);
   }
 }
 
-if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit("acode.pinch", async baseUrl => {
-    if (!baseUrl.endsWith("/")) {
-      baseUrl += "/";
-    }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init();
-  });
-  acode.setPluginUnmount("acode.pinch", () => {
-    acodePlugin.destroy();
-  });
-}
+const acodePlugin = new AcodePlugin();
+acode.setPluginInit("acode.pinch", () => acodePlugin.init());
+acode.setPluginUnmount("acode.pinch", () => acodePlugin.destroy());
